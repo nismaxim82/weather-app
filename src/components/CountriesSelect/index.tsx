785147c177fb278d507bb5c00fbe5444ef5bc53c
Maxim Nisenblat <P0024891@ness-tech.co.il>
2020-06-25 09:15:41 +0300

    add mobx, material autocomplete

--- src/components/CountriesSelect/index.tsx
+++ src/components/CountriesSelect/index.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
-import { FormControl, InputLabel, Input, Icon } from '@material-ui/core';
+import { FormControl, TextField, InputAdornment } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
+import { Search as SearchIcon } from '@material-ui/icons';
 import styles from './index.module.css';
 
-interface IProps {}
+interface IProps { }
 
 export default (props: IProps) => {
-  const search = (countryName: string) => {
-    console.log(countryName);
+  const [selectText, setSelectText] = React.useState('');
+  const search = (event: any) => {
+    setSelectText(event.currentTarget.value);
+  };
+
+  React.useEffect(() => {
+    console.log(selectText);
+  }, [selectText]);
+
+  const [countries, setCountries] = React.useState([
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+  ]);
+
+  const selectCountryTextField = (params: any) => {
+    const inputProps = params.InputProps || {};
+    inputProps.startAdornment =
+      <InputAdornment position="start">
+        <SearchIcon />
+      </InputAdornment>
+    return (
+      <TextField
+        {...params} label="Select a country" variant="outlined"
+        InputProps={inputProps}
+      />
+    )
   };
 
   return (
     <div className={styles.container}>
-      <h1>test</h1>
       <FormControl fullWidth>
-        <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
-        <Input
-          id="standard-adornment-amount"
-          // value={values.amount}
-          // onChange={handleChange('amount')}
-          startAdornment={<Icon>Search</Icon>}
+        <Autocomplete
+          loading
+          open={!!selectText}
+          options={countries}
+          getOptionLabel={(option: any) => option.title}
+          renderInput={selectCountryTextField}
+          onInputChange={search}
         />
       </FormControl>
     </div>
